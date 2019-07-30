f1c577dba9d309cae444c110191a7f2ec990c3c1
root <root@ctl.gw.lo>
2019-07-30 15:12:51 -0500

    Cleanup

--- erasedisk.sh
+++
@@ -1,3 +0,0 @@
-
-ssh root@esx.gw.lo "cd /vmfs/volumes/datastore1/$1;rm $1.vmdk;rm $1.vmsd;rm $1-flat.vmdk;vmkfstools --createvirtualdisk 160G --diskformat thin $1.vmdk"
-
--- poweroff-all-vms.sh
+++
@@ -1,9 +0,0 @@
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 7
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 8
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 9
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 10
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 11
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 12
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 13
-ssh root@esx.gw.lo vim-cmd vmsvc/power.off 15
-

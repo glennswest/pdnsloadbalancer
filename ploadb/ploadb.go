5dfc428092593061a297f3e08698c9665de65472
gwest <gwest@redhat.com>
2019-07-31 11:26:57 -0500

    Add service support

--- ploadb/ploadb.go
+++ ploadb/ploadb.go
@@ -2,12 +2,66 @@ package main
 
 import "github.com/tidwall/sjson"
 import "github.com/tidwall/gjson"
+import "os"
 import "github.com/go-resty/resty"
 //import "fmt"
 import "time"
 import ping "github.com/sparrc/go-ping"
 import "strconv"
 import "log"
+import "gopkg.in/natefinch/lumberjack.v2"
+import "github.com/kardianos/service"
+ 
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	// Do work here
+        DoWork()
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func main() {
+	// Start should not block. Do the actual work async.
+        log.SetOutput(&lumberjack.Logger{
+                                  Filename:   "/var/log/ploadb/ploadb.log",
+                                  MaxSize:    5, // megabytes
+                                  MaxBackups: 3,
+                                  MaxAge:     28, //days
+                                  Compress:   true, // disabled by default
+                                 })
+	svcConfig := &service.Config{
+		Name:        "ploadb",
+		DisplayName: "Monitor hostnames with multiple ip and disable when down,enable when up",
+		Description: "PowerDNSLoadBalancer",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Printf("Fatal: %v\n",err)
+                return
+	}
+        if len(os.Args) > 1 {
+		err = service.Control(s, os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	   }
+	err = s.Run()
+	if err != nil {
+               log.Printf("Error: %v\n",err)
+               return
+	}
+}
 
 
 func getdomain(domain string) string{
@@ -33,6 +87,29 @@ func getdomain(domain string) string{
         return(resp.String())
 }
 
+func getdomainlist() string{
+      // Create a Resty Client
+       client := resty.New()
+       resp, _ := client.R().
+           SetHeaders(map[string]string{
+                      "Content-Type": "application/json",
+                       "X-API-KEY": "Secret2018"}).
+           Get("http://ctl.gw.lo:8081/api/v1/servers/localhost/zones")
+        // Explore response object
+        /*
+        fmt.Println("Response Info:")
+        //fmt.Println("Error      :", err)
+        fmt.Println("Status Code:", resp.StatusCode())
+        fmt.Println("Status     :", resp.Status())
+        fmt.Println("Time       :", resp.Time())
+        fmt.Println("Received At:", resp.ReceivedAt())
+        fmt.Println("Body       :\n", resp)
+        fmt.Println()
+        */
+        data := resp.String()
+        zones := gjson.Get(data,"#.name").String()
+        return(zones)
+}
 
 func handle_load_balance(domain string,name string,count int,records string){
 
@@ -103,13 +180,13 @@ func send_update(domain string,name string,records string) string{
 
 }
 
-func main(){
+func DoWork(){
 
-        for true {
-            domain := "gw.lo."
-            process_domain(domain)
-            time.Sleep(10 * time.Second)
-            }
+        domainsjs := getdomainlist()
+        domains := gjson.Parse(domainsjs).Array()
+        for _,domain := range domains{
+              process_domain(domain.String())
+              }
 }
 
 func process_domain(domain string){
